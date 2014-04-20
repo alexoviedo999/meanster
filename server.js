0a71017de4c11af823c37341cbb8a1d562f37dc1
alexoviedo999 <alexoviedo999@gmail.com>
2014-04-19 22:20:01 -0400

    Added middleware. added stylus for css. server.js is now running hello world.

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var express = require('express');
+    stylus = require('stylus');
+    logger = require('morgan');
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.configure(function () {
-  app.set('views', __dirname + '/server/views');
-  app.set('view engine', 'jade');
-});
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade'); 
+app.use(logger);
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+  console.log(req.body) // populated!
+  next();
+})
 
 app.get('*', function( req, res)  {
   res.render('index');
